8a02324874ce69c0860fae11578b1ab5cd3605cc
Steven <onojasteven13@gmail.com>
2023-11-15 11:46:05 +0100

    5th commit

---
+++ backend/routes/tickeRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router  = express.Router()
+const { createTicket, getTickets, getSingleTicket, deleteTicket, updateTicket } = require('../controllers/ticketController')
+
+const {protect} = require('../middleware/authMiddleware') 
+
+router.route('/').get(protect, getTickets).post(protect, createTicket)
+
+router.route('/:id').get(protect, getSingleTicket).delete(protect, deleteTicket).put(protect, updateTicket)
+
+module.exports = router
